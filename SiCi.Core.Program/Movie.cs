f0b32ccc1647fda6c1330481adcde0b36d9a0d53
Ashijo <gpasquet412@gmail.com>
2023-05-27 16:28:51 -0400

    meili search hello world

---
+++ SiCi.Core.Program/Movie.cs
@@ -0,0 +1,8 @@
+namespace SiCi.Core.Program;
+
+public class Movie
+{
+	public string Id { get; set; }
+	public string Title { get; set; }
+	public string[] Genres { get; set; }
+}
--- SiCi.Core.Program/Program.cs
+++ SiCi.Core.Program/Program.cs
@@ -1,3 +1,7 @@
+using System.Text.Json;
+using Meilisearch;
+using SiCi.Core.Program;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -21,5 +25,32 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+
+
+MeilisearchClient client = new MeilisearchClient("http://localhost:7700", "masterKey");
+var options = new JsonSerializerOptions
+{
+	PropertyNameCaseInsensitive = true
+};
+
+// An index is where the documents are stored.
+var index = client.Index("movies");
+var documents = new Movie[] {
+	new Movie { Id = "1", Title = "Carol", Genres = new string[] { "Romance", "Drama" }  },
+	new Movie { Id = "2", Title = "Wonder Woman", Genres = new string[] { "Action", "Adventure" } },
+	new Movie { Id = "3", Title = "Life of Pi", Genres = new string[] { "Adventure", "Drama" } },
+	new Movie { Id = "4", Title = "Mad Max: Fury Road", Genres = new string[] { "Adventure", "Science Fiction"} },
+	new Movie { Id = "5", Title = "Moana", Genres = new string[] { "Fantasy", "Action" } },
+	new Movie { Id = "6", Title = "Philadelphia", Genres = new string[] { "Drama" } }
+};
+
+await index.AddDocumentsAsync<Movie>(documents);
+
+var movies = await index.SearchAsync<Movie>("a");
+foreach(var prop in movies.Hits) {
+	Console.WriteLine (prop.Title);
+}
+
 
 app.Run();
